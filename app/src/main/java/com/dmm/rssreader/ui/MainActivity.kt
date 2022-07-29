b7612e92b36e17a5afdb857fa6f4b4139f1eaf8f
Daniel Moreno Marquez <daniel-moreno-marquez@hotmail.es>
2022-07-29 10:23:11 +0300

    Delete Share Options, set Title app bar with title feed selected

--- app/src/main/java/com/dmm/rssreader/ui/MainActivity.kt
+++ app/src/main/java/com/dmm/rssreader/ui/MainActivity.kt
@@ -1,41 +1,42 @@
 package com.dmm.rssreader.ui
 
-import android.content.res.Configuration
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.dmm.rssreader.R
 import com.dmm.rssreader.databinding.ActivityMainBinding
+import com.dmm.rssreader.ui.viewModel.MainViewModel
 import com.dmm.rssreader.utils.Utils.Companion.isNightMode
 import dagger.hilt.android.AndroidEntryPoint
-import java.security.AccessController.getContext
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
 	private lateinit var binding: ActivityMainBinding
+	private lateinit var viewModel: MainViewModel
 	lateinit var navController: NavController
 
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		binding = ActivityMainBinding.inflate(layoutInflater)
+		viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 		setContentView(binding.root)
 
 		val navHostFragment = supportFragmentManager
-			.findFragmentById(com.dmm.rssreader.R.id.fragment_container) as NavHostFragment
+			.findFragmentById(R.id.fragment_container) as NavHostFragment
 		navController = navHostFragment.navController
 
-		val appConfiguration = AppBarConfiguration(setOf(com.dmm.rssreader.R.id.homeFragment, com.dmm.rssreader.R.id.readLaterFragment, com.dmm.rssreader.R.id.settingsFragment))
+		val appConfiguration = AppBarConfiguration(setOf(R.id.homeFragment, R.id.readLaterFragment, R.id.settingsFragment))
 		binding.toolbar.setupWithNavController(navController, appConfiguration)
 		binding.bottomNavigation.setupWithNavController(navController)
 
 		setSupportActionBar(binding.toolbar)
 		destinationChangedListener()
 		setShadowColor()
-
 	}
 
 	private fun destinationChangedListener() {
@@ -51,7 +52,7 @@ class MainActivity : AppCompatActivity() {
 					setTitleMateriaToolbar(R.string.title_settings_fragment)
 				}
 				R.id.feedDescriptionFragment -> {
-					//setTitleMateriaToolbar(R.string.title_readlater_fragment)
+					binding.toolbar.title = viewModel.feedSelected.title
 				}
 			}
 		}
