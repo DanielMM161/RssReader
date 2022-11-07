38bf1164a3e2b17518d118972f21620ea96f2617
Daniel Moreno Marquez <daniel-moreno-marquez@hotmail.es>
2022-11-07 18:21:25 +0200

    Changue styles from splash screen

--- app/src/main/java/com/dmm/rssreader/presentation/activities/SplashScreenActivity.kt
+++ app/src/main/java/com/dmm/rssreader/presentation/activities/SplashScreenActivity.kt
@@ -1,33 +1,17 @@
 package com.dmm.rssreader.presentation.activities
 
-import android.app.ActivityOptions
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
-import android.util.Log
-import android.util.Pair
-import android.view.View
-import android.view.WindowManager
-import android.view.animation.Animation
-import android.view.animation.AnimationUtils
-import android.widget.EditText
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
+import android.os.Looper
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.dmm.rssreader.R
 import com.dmm.rssreader.databinding.ActivitySplashScreenBinding
-import com.dmm.rssreader.domain.extension.Error
-import com.dmm.rssreader.domain.extension.gone
 import com.dmm.rssreader.domain.model.UserProfile
 import com.dmm.rssreader.presentation.viewModel.AuthViewModel
 import com.dmm.rssreader.utils.Constants
 import com.dmm.rssreader.utils.Resource
-import com.dmm.rssreader.utils.Utils
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInClient
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -47,12 +31,12 @@ class SplashScreenActivity : AppCompatActivity() {
 		setContentView(binding.root)
 		binding.loadingFeedback.text = getString(R.string.checking_credentials)
 
-		checkIfUserAuthenticated()
+		Handler(Looper.getMainLooper()).postDelayed({
+			checkIfUserAuthenticated()
+		}, SPLASH_SCREEN)
 	}
 
-
 	private fun checkIfUserAuthenticated() {
-		binding.loadingFeedback.text = getString(R.string.upload_data)
 		authViewModel.checkIfUserIsAuthenticatedInFireBase()
 		authViewModel.authUser.observe(this) { user ->
 			if(!user.isAuthenticated) {
@@ -64,13 +48,13 @@ class SplashScreenActivity : AppCompatActivity() {
 	}
 
 	private fun getUserDocument(documentPath: String) {
+		binding.loadingFeedback.text = getString(R.string.upload_data)
 		authViewModel.getUserDocument(documentPath)
 		authViewModel.currentUser.observe(this) { it ->
 			when(it) {
 				is Resource.Success -> {
 					val user = it.data
 					if(user != null) {
-						binding.loadingFeedback.text = getString(R.string.going_home_screen)
 						goToMainActivity(user)
 					}
 				}
@@ -95,19 +79,5 @@ class SplashScreenActivity : AppCompatActivity() {
 		val intent = Intent(this, AuthActivity::class.java)
 		startActivity(intent)
 		finish()
-		//finish()
-//		Handler().postDelayed({
-//			val intent = Intent(this, AuthActivity::class.java)
-//
-//			val pairs: Array<Pair<View, String>?> = arrayOfNulls(1)
-//			pairs[0] = Pair<View, String>(binding.imageView, "logo_image")
-//
-//			if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-//				var options = ActivityOptions.makeSceneTransitionAnimation(this, *pairs)
-//				startActivity(intent, options.toBundle())
-//			} else {
-//				startActivity(intent)
-//			}
-//		}, SPLASH_SCREEN)
 	}
 }
--- app/src/main/java/com/dmm/rssreader/presentation/fragments/auth/LoginFragment.kt
+++ app/src/main/java/com/dmm/rssreader/presentation/fragments/auth/LoginFragment.kt
@@ -200,21 +200,6 @@ class LoginFragment : Fragment() {
 
 	private fun goToRegister() {
 		binding.signupBtn.setOnClickListener {
-//			val intent = Intent(this, RegisterActivity::class.java)
-//
-//			val pairs: Array<Pair<View, String>?> = arrayOfNulls(7)
-//			pairs[0] = Pair<View, String>(binding.logo, "logo_image")
-//			pairs[1] = Pair<View, String>(binding.logoText, "logo_title")
-//			pairs[2] = Pair<View, String>(binding.signinText, "logo_subtitle")
-//			pairs[3] = Pair<View, String>(binding.username, "username_tran")
-//			pairs[4] = Pair<View, String>(binding.password, "password_tran")
-//			pairs[5] = Pair<View, String>(binding.loginBtn, "button_tran")
-//			pairs[6] = Pair<View, String>(binding.signupBtn, "login_signup_tran")
-
-//			if(android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
-//				var options = ActivityOptions.makeSceneTransitionAnimation(this, *pairs)
-//				startActivity(intent, options.toBundle())
-//			}
 			navigate(R.id.action_loginFragment_to_registerFragment)
 		}
 	}
